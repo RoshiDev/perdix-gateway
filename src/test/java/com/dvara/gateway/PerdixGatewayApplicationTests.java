bdc0f625ad4c28ea281823ee496ad36c773a2f66
rajesh.kumar <Rajesh.Kumar@dvara.com>
2022-01-12 12:38:19 +0530

    initital coomit

---
+++ src/test/java/com/dvara/gateway/PerdixGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvara.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerdixGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
